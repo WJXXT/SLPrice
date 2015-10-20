9d001245e8e08d8918de859fab46291e67866086
WJXXT <495820035@qq.com>
2015-10-20 09:29:28 +0800

    Initial Commit

---
+++ SLPrice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLPrice
+//
+//  Created by XXT on 15/10/20.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
